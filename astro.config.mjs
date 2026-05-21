49c45ba6ec9b76e5adc2a85ba51729c3b03faa3b
Adrian <adriansanchezquijada@gmail.com>
2026-05-21 21:52:12 +0100

    chore: add preact + tailwind + mdx integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
