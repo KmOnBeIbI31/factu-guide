c3ecd1a5dc1ca22cceb44597c14cc6a2a84e5f4d
Adrian <adriansanchezquijada@gmail.com>
2026-05-21 21:56:16 +0100

    chore: configure Biome for lint and format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), mdx()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
